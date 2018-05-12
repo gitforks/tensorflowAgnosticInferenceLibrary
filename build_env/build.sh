bc32029fed2ac5185d097ebbb7c66b560045bfcd
Jan <martin-jan@arcor.de>
2018-05-12 12:17:34 +0200

    adding copying instructions

--- build_env/build.sh
+++ build_env/build.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+BIN_DST_DIR=/home/tf_bin/
+
 mkdir /home/tf && cd /home/tf
 
 git clone https://github.com/tensorflow/tensorflow 
@@ -16,4 +18,10 @@ cp -R src/* /home/tf/tensorflow/tensorflow/examples/
 cd /home/tf/tensorflow
 
 ./configure
+
+bazel build --config=opt --config=monolithic //tensorflow/examples/tf_inference_lib:tf_inference_lib
+cp bazel-bin/tensorflow/examples/tf_inference_lib/libtf_inference_lib.so $BIN_DST_DIR
+
+bazel build --config=opt --config=monolithic //tensorflow:libtensorflow_cc.so
+cp bazel-bin/tensorflow/libtensorflow_*.so $BIN_DST_DIR
 
