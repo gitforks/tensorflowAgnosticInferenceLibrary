29c6b483141ada8718eb97daf4d6b9c40b5e81b9
x42x64 <martin-jan@arcor.de>
2018-05-21 19:28:25 +0000

    Adding build targets for distribution

--- build_env/build.sh
+++ build_env/build.sh
@@ -19,9 +19,25 @@ cd /home/tf/tensorflow
 
 ./configure
 
-bazel build --config=opt --config=monolithic //tensorflow/examples/tf_inference_lib:libtf_inference_lib.so
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/examples/tf_inference_lib:libtf_inference_lib.so
 cp bazel-bin/tensorflow/examples/tf_inference_lib/libtf_inference_lib.so $BIN_DST_DIR
+cp tensorflow/examples/tf_inference_lib/*.h $BIN_DST_DIR
 
-#bazel build --config=opt --config=monolithic //tensorflow:libtensorflow_cc.so
-#cp bazel-bin/tensorflow/libtensorflow_*.so $BIN_DST_DIR
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/contrib/tensorrt:python/ops/_trt_engine_op.so
+cp bazel-bin/tensorflow/contrib/tensorrt/python/ops/_trt_engine_op.so $BIN_DST_DIR
+
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/tools/graph_transforms:transform_graph
+cp bazel-bin/tensorflow/tools/graph_transforms/transform_graph $BIN_DST_DIR
+
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/tools/graph_transforms:summarize_graph
+cp bazel-bin/tensorflow/tools/graph_transforms/summarize_graph $BIN_DST_DIR
+
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/tools/graph_transforms:compare_graphs
+cp bazel-bin/tensorflow/tools/graph_transforms/compare_graphs $BIN_DST_DIR
+
+bazel build --config=opt --config=cuda --config=monolithic //tensorflow/tools/benchmark:benchmark_model
+cp bazel-bin/tensorflow/tools/benchmark/benchmark_model $BIN_DST_DIR
+
+bazel build --config=opt --config=monolithic //tensorflow:libtensorflow_cc.so
+cp bazel-bin/tensorflow/libtensorflow_*.so $BIN_DST_DIR
 
